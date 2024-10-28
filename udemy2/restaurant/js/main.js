4aad57ad5c24a31cc9d05657fe5d34e121bdc588
Kaarel <viilvere.k@gmail.com>
2024-10-28 18:03:08 +0200

    resto

---
+++ udemy2/restaurant/js/main.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () => {
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        navbarCollapse.classList.remove('show');
+    });
+});
